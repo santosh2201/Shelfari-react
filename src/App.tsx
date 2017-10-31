fcae2e6ccd1f12d2afbbff2ec59166cc8ca2c01d
Santosh Reddy Nancharla <santosh.cool.reddy@gmail.com>
2017-10-31 17:44:09 -0400

    Created shelfari app using react

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Shelfari</h2>
+        </div>
+        <p className="App-content">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
