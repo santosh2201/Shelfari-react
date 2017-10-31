67eab7037534d84e737ad532f896028201c91cbf
Santosh Reddy Nancharla <santosh.cool.reddy@gmail.com>
2017-10-31 19:00:27 -0400

    Included add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 class App extends React.Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Shelfari</h2>
-        </div>
-        <p className="App-content">
-          
-        </p>
-      </div>
-    );
+      return (
+          <div className="App">
+              <div className="App-header">
+                  <h2>Shelfari</h2>
+              </div>
+              <div className="App-content">
+                  <div className="add-book-container">
+                      <Button bsStyle="primary" bsSize="large" block>Add</Button>
+                  </div>
+              </div>
+          </div>
+      );
   }
 }
 
